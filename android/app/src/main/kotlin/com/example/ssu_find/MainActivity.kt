36567a703eaef3ddadff5861fe6be5af71087144
Nathasia <abatayo.nathasia@gmail.com>
2025-07-01 00:56:21 +0800

    Built web
    Branch: main
    Commit: 5d3e4112df81e305f9a86fddcbdc3e3b033af4ed (dirty)
    
    package:peanut 6.0.0

--- android/app/src/main/kotlin/com/example/ssu_find/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ssu_find
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "70bbfd61f2092370de66156bfd56d5c0",
+"flutter_bootstrap.js": "8473895ec99aae7e23e86191020bbcd8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c7111660ec7a17fe543b76f89de48abf",
-"/": "c7111660ec7a17fe543b76f89de48abf",
-"main.dart.js": "61db8f2ca8fcf11b5b8c6ef3520769b8",
+"index.html": "a5402298cd9cef1fb5be782913b7a453",
+"/": "a5402298cd9cef1fb5be782913b7a453",
+"main.dart.js": "0aa456d75e21da4032c2b19eabcaa344",
 "manifest.json": "9c42cfae7d7798e6081e0421dc7191a8",
 "version.json": "4041f227c94c17d2a0e9eb9f77423935"};
 // The application shell files that are downloaded before a service worker can
