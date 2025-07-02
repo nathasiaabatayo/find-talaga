fb3416f99e05254ac4431a46d97e35719eeb8194
Nathasia <abatayo.nathasia@gmail.com>
2025-07-02 22:23:46 +0800

    Built web
    Branch: main
    Commit: 4190462be8830ea57bdd5803ed0d02c8b03181f8 (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1cec29c6a0a66289aadbe7c0caf3f38e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "403512bd5bdce2cc81a46c4f749723cd",
+"flutter_bootstrap.js": "7112e7f0a5ae0a88a7382cf4d5ec93aa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
